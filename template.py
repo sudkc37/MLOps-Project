fed970367cb804839c4095cf869f1ffab311024a
Sudip Khadka <sudipkhadka@Sudips-MBP.lan>
2024-09-24 16:49:07 -0500

    folder stracture added

--- template.py
+++ template.py
@@ -8,6 +8,7 @@ project_name = "mlProject"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
+    f"src/__init__.py",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
